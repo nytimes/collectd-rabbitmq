e22f93063af9ae3168be7e146ad46b8cdad7f924
jimbydamonk <mike.buzzetti@gmail.com>
2016-04-28 14:38:40 -0400

    Bump version: 1.4.0 → 1.5.0

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ test_requirements = [
 
 setup(
     name='collectd-rabbitmq',
-    version='1.4.0',
+    version='1.5.0',
     description="A collected plugin, written in python, to"
                 "collect statistics from RabbitMQ.",
     long_description=readme + '\n\n' + history,
